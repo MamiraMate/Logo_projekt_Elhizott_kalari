62a13dbfbc81eceeeb67fbe71539f67e97417e00
Mamira Máté <mamira.mate.21f@szlgbp.hu>
2022-03-01 19:47:21 +0100

    összes feladat megadva

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,22 @@ namespace LogoKaresz
 		{
 			/* Ezt indítja a START gomb! */
 			// Teleport(közép.X, közép.Y+150, észak);
+
+
+			/// lehetséges feladatok
+
+			/* széle: Máté
+			 * tetején lévő madár: 
+			 * várnak a torony része (amelyik kilóg a téglalapból):
+			 * felül lévő virág a vár mellett:
+			 * vár ajtaja: 
+			 * középen lévő cik-cak:
+			 * középen lévő hal feje:
+			 * középen lévő hal teste:
+			 * alul lévő zöld-fehér kerek virág:
+			 * alul lévő zöld-fehér elnyújtott virág:
+			 * alul lévő fehér magas virág:
+			 */
 		}
 	}
 }
